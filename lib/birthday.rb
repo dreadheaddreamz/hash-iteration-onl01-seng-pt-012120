ed05b9d7c15e55e0b031e8167c33d1fc76f1a096
James Batsell <jamesbatsell05@gmail.com>
2020-02-02 21:00:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,15 +9,15 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-def age_appropriate_birthday
-  birthday_kids.each do |kids_name, age|
-    if age <= 12 
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-      elsif age >= 12
-      puts "Happy Birthday #{kids_name}! but you're too old for this message."
-    end
+#def age_appropriate_birthday
+ # birthday_kids.each do |kids_name, age|
+  #  if age <= 12 
+   #   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    #  elsif age >= 12
+     # puts "Happy Birthday #{kids_name}! but you're too old for this message."
+    #end
       
-end
+#end
   
 #def happy_birthday_under_12(birthday_kid)
  # birthday_kid.each do |kids_name, age|
